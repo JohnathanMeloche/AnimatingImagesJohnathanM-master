2d9feb399e0ac8ef0934356839a3a7c3fcc35fe1
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2018-03-29 19:49:01 -0400

    s

--- main.lua
+++ main.lua
@@ -3,179 +3,81 @@
 -- Johnathan M ICS20
 -- Animating Images
 
+-- hide the status bar
+display.setStatusBar (display.HiddenStatusBar)
+
 local backgroundimage = display.newImageRect("Images/background.jpg", 1960,1500)
 	
 	--centers the background image
 	backgroundimage.x = display.contentWidth/2
 	backgroundimage.y = display.contentWidth*76/100
 
-
 local blueCar = display.newImageRect("Images/bluecar.png", 480, 200)
 	blueCar.x = 500
 	blueCar.y = 1100
-
-
 
 local redCar = display.newImageRect("Images/redcar.png", 480, 200)
 	redCar.x = 500
 	redCar.y = 1250
 
+local moveSpeed = 20
+local moveSpeed2 = 40
+redCar.id =  "redCar"
 
 
--- hide the status bar
-display.setStatusBar (display.HiddenStatusBar)
+
+
+
+
+
+
+local function moveRed(event)
+	
+	if ( event.phase == "began" ) then
+
+	redCar.x = redCar.x + moveSpeed2
+
+		if ( redCar.x > 900 ) then
+
+			moveSpeed2 = -40
+
+		elseif ( redCar.x < 0 ) then
+
+			moveSpeed2 = 40
+
+
+		end
+
+	end
+
+end
+
+
 
 --local function created for Object1
-local function MoveObject1(event)
+local function moveBlue()
 
+	blueCar.x = blueCar.x + moveSpeed
 	
+	if ( blueCar.x > 900 ) then
 
-end
+		moveSpeed = -20
 
+	elseif ( blueCar.x < 0 ) then
 
-Runtime:addEventListener("enterFrame", MoveObject1)
+			moveSpeed = 40
 
---[[
---local function created for Object2	
-local function MoveObject2(event)
-
-end
-
-Runtime:addEventListener("enterFrame" MoveObject2)
-
---local function created for Object3	
-local function MoveObject3(event)
-
-end
-
-Runtime:addEventListener("enterFrame" MoveObject3)
-
-]]--
-
-
-
-
-
-
-
-
-
-
-
---[[
---global variable
-scrollSpeed = 5
-
-scrollSpeed2 = 12
-
-scrollSpeed3 = 7
-
---background image
-local backgroundImage = display.newImageRect("background.jpg", 1960, 1960)
-
-	--centers the background image
-	backgroundImage.x = display.contentWidth/2
-	backgroundImage.y = display.contentWidth/2
-
-local rocket = display.newImageRect("beetleship.png", 450, 540)
-
-	--sets image1 position
-	rocket.x = 0
-	rocket.y = 700
-
-
-local function MoveShip(event)
-	-- links the scroll speed to the x value of rocket
-	rocket.x = rocket.x + scrollSpeed
-
-
-	--flips rocket when it gets off screen
-	if (rocket.x > 1200) then
-
-		rocket.xScale = -1 
-
-		--moves rocket back across screen once its off screen
-		scrollSpeed = -5 
-	end
-
-	if (rocket.x < -200) then
-
-		rocket.xScale = 1
-
-		scrollSpeed = 5
 	end
 
 end
 
--- MoveShip will be called
-Runtime:addEventListener("enterFrame", MoveShip)
 
 
-local shark = display.newImageRect("shark.gif", 450, 540)
-
-	--sets shark position
-	shark.x = 0
-	shark.y = 1000
 
 
-local function MoveShark(event)
-	-- links the scroll speed to the x value of shark
-	shark.x = shark.x + scrollSpeed2
 
 
-	--flips shark when it gets off screen
-	if (shark.x > 1200) then
 
-		shark.xScale = -1 
-
-		--moves shark back across screen once its off screen
-		scrollSpeed2 = -12 
-	end
-
-	if (shark.x < -200) then
-
-		shark.xScale = 1
-
-		scrollSpeed2 = 12
-	end
-
-end
-
--- MoveShark will be called
-Runtime:addEventListener("enterFrame", MoveShark)
-
-local crest = display.newImageRect ("crest.png", 420, 550)
-	--sets crest position
-	crest.x = 600
-	crest.y = 100
-	
-
-
-local function MoveCrest(event)
-	-- links the scroll speed to the x value of crest
-	crest.y = crest.y + scrollSpeed3
-
-
-	--flips crest when it gets off screen
-	if (crest.y > 1200) then
-
-
-		--moves crest back across screen once its off screen
-		scrollSpeed3 = -7
-	end
-
-	if (crest.y < 250) then
-
-
-		scrollSpeed3 = 7
-	end
-
-end
-
--- MoveShip will be called
-Runtime:addEventListener("enterFrame", MoveCrest)
-
-
-]]--
-
-
+--Runtime:addEventListener("enterframe", moveBlue)
+timer.performWithDelay(500, moveBlue, 0)
+redCar:addEventListener("touch", moveRed)
